61b6827659962444c7b6e3ff42efb37afa7699bb
abdulahadnewgmail-cmyk <abdul.ahad.new.gmail@gmail.com>
2026-03-15 22:09:33 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,10 +11,32 @@ const assets = [
   './table.js'
 ];
 
+// Install: Cache all files
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  self.skipWaiting(); // Forces the waiting service worker to become active
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
+// Activate: Clean up old caches
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch: Serve from cache, fallback to network
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
